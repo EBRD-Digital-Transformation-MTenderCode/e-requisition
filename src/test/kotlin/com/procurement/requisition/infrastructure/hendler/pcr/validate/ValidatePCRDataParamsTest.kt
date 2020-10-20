a91bbde12f7fc7901f5bbdcfc595855d2e7fc875
Maxim Sambulat <maxim.sambulat@gmail.com>
2020-10-20 19:25:42 +0300

    ES-4492 Renamed and moved json for test 'validatePCRData' command.

--- src/test/kotlin/com/procurement/requisition/infrastructure/hendler/pcr/validate/ValidatePCRDataParamsTest.kt
+++ src/test/kotlin/com/procurement/requisition/infrastructure/hendler/pcr/validate/ValidatePCRDataParamsTest.kt
@@ -8,16 +8,16 @@ class ValidatePCRDataParamsTest {
 
     @Test
     fun fully() {
-        testingBindingAndMapping<ValidatePCRDataParams>("json/infrastructure/web/dto/pcr_full.json")
+        testingBindingAndMapping<ValidatePCRDataParams>("json/infrastructure/handler/pcr/validate/request_validate_pcr_data_full.json")
     }
 
     @Test
     fun required1() {
-        testingBindingAndMapping<ValidatePCRDataParams>("json/infrastructure/web/dto/pcr_required_1.json")
+        testingBindingAndMapping<ValidatePCRDataParams>("json/infrastructure/handler/pcr/validate/request_validate_pcr_data_required_1.json")
     }
 
     @Test
     fun required2() {
-        testingBindingAndMapping<ValidatePCRDataParams>("json/infrastructure/web/dto/pcr_required_2.json")
+        testingBindingAndMapping<ValidatePCRDataParams>("json/infrastructure/handler/pcr/validate/request_validate_pcr_data_required_2.json")
     }
 }
