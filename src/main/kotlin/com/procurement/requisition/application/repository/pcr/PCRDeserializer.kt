3dbaa38f4cefbd0b0e86b1ef783b582aa5ba8ae3
Maxim Sambulat <maxim.sambulat@gmail.com>
2020-10-24 19:29:35 +0300

    ES-4609 Fixed name attribute 'owner' from 'token' to 'owner'. Added replacing error in 'PCRDeserializer' & 'PCRDeserializer'.

--- src/main/kotlin/com/procurement/requisition/application/repository/pcr/PCRDeserializer.kt
+++ src/main/kotlin/com/procurement/requisition/application/repository/pcr/PCRDeserializer.kt
@@ -5,5 +5,5 @@ import com.procurement.requisition.lib.fail.Failure
 import com.procurement.requisition.lib.functional.Result
 
 interface PCRDeserializer {
-    fun build(json: String): Result<PCR, Failure.Error>
+    fun build(json: String): Result<PCR, Failure>
 }
--- src/main/kotlin/com/procurement/requisition/infrastructure/repository/pcr/PCRSerializerImpl.kt
+++ src/main/kotlin/com/procurement/requisition/infrastructure/repository/pcr/PCRSerializerImpl.kt
@@ -2,14 +2,21 @@ package com.procurement.requisition.infrastructure.repository.pcr
 
 import com.procurement.requisition.application.repository.pcr.PCRSerializer
 import com.procurement.requisition.application.service.Transform
-import com.procurement.requisition.domain.failure.error.TransformErrors
+import com.procurement.requisition.domain.failure.incident.InternalServerError
 import com.procurement.requisition.domain.model.PCR
 import com.procurement.requisition.infrastructure.repository.pcr.model.serialization
+import com.procurement.requisition.lib.fail.Failure
 import com.procurement.requisition.lib.functional.Result
 import org.springframework.stereotype.Component
 
 @Component
 class PCRSerializerImpl(val transform: Transform) : PCRSerializer {
-    override fun build(pcr: PCR): Result<String, TransformErrors.Serialization> =
+    override fun build(pcr: PCR): Result<String, Failure> =
         transform.trySerialization(pcr.serialization())
+            .mapFailure { failure ->
+                InternalServerError(
+                    description = "Error of serialization 'PCREntity' to JSON.",
+                    reason = failure.reason
+                )
+            }
 }
