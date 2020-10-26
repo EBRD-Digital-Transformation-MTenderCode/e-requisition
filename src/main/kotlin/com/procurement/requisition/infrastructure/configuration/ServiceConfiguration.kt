b7822d10cbd2843858a140841ae2038b7c043317
Evgen Kopach <zkopach@gmail.com>
2020-10-26 17:19:11 +0200

    ES-4652 Fixed incorrect base scan path.

--- src/main/kotlin/com/procurement/requisition/infrastructure/configuration/ServiceConfiguration.kt
+++ src/main/kotlin/com/procurement/requisition/infrastructure/configuration/ServiceConfiguration.kt
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Configuration
 @Configuration
 @ComponentScan(
     basePackages = [
-        "com.procurement.requisition.service",
         "com.procurement.requisition.application.service",
     ]
 )
