15862c8d8c024114075d7f3892c22e60b1e5656a
Maxim Sambulat <maxim.sambulat@gmail.com>
2020-10-26 18:22:59 +0200

    ES-4573 Moved tests and json files.

---
+++ src/test/kotlin/com/procurement/requisition/infrastructure/hendler/v2/pcr/validate/ValidatePCRDataRequestTest.kt
@@ -0,0 +1,23 @@
+package com.procurement.requisition.infrastructure.hendler.v2.pcr.validate
+
+import com.procurement.requisition.infrastructure.handler.v2.pcr.validate.model.ValidatePCRDataRequest
+import com.procurement.requisition.json.testingBindingAndMapping
+import org.junit.jupiter.api.Test
+
+class ValidatePCRDataRequestTest {
+
+    @Test
+    fun fully() {
+        testingBindingAndMapping<ValidatePCRDataRequest>("json/infrastructure/handler/v2/pcr/validate/request_validate_pcr_data_full.json")
+    }
+
+    @Test
+    fun required1() {
+        testingBindingAndMapping<ValidatePCRDataRequest>("json/infrastructure/handler/v2/pcr/validate/request_validate_pcr_data_required_1.json")
+    }
+
+    @Test
+    fun required2() {
+        testingBindingAndMapping<ValidatePCRDataRequest>("json/infrastructure/handler/v2/pcr/validate/request_validate_pcr_data_required_2.json")
+    }
+}
