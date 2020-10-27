f96e4605051e80e9db74331e41addef1d39a66a6
Maxim Sambulat <maxim.sambulat@gmail.com>
2020-10-27 15:14:17 +0200

    ES-4663 Fixed problem of invalid response json for 'getActiveLots' command.

--- src/main/kotlin/com/procurement/requisition/infrastructure/handler/v1/get/lot/model/ToActiveLotIdsResponseConverter.kt
+++
@@ -1,9 +0,0 @@
-package com.procurement.requisition.infrastructure.handler.v1.get.lot.model
-
-import com.procurement.requisition.application.service.get.lot.model.ActiveLotIds
-
-fun ActiveLotIds.convert() = GetActiveLotIdsResponse(
-    lots = this.lots.map { lot -> lot.convert() }
-)
-
-fun ActiveLotIds.Lot.convert(): GetActiveLotIdsResponse.Lot = GetActiveLotIdsResponse.Lot(id = id)
