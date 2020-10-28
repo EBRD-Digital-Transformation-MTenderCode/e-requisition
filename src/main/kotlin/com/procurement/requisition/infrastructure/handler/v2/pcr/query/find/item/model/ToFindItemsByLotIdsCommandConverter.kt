fb6c87bebc71161fe7c05436ba6e7a7efdff47a5
Evgen Kopach <zkopach@gmail.com>
2020-10-28 13:15:33 +0200

    ES-4529 Refactored code. Split convertation into small functions.

--- src/main/kotlin/com/procurement/requisition/infrastructure/handler/v2/pcr/query/find/item/model/ToFindItemsByLotIdsCommandConverter.kt
+++ src/main/kotlin/com/procurement/requisition/infrastructure/handler/v2/pcr/query/find/item/model/ToFindItemsByLotIdsCommandConverter.kt
@@ -5,6 +5,7 @@ import com.procurement.requisition.domain.failure.error.JsonErrors
 import com.procurement.requisition.domain.model.Cpid
 import com.procurement.requisition.domain.model.Ocid
 import com.procurement.requisition.domain.model.tender.lot.LotId
+import com.procurement.requisition.lib.failureIfEmpty
 import com.procurement.requisition.lib.functional.Result
 import com.procurement.requisition.lib.functional.Result.Companion.failure
 import com.procurement.requisition.lib.functional.asSuccess
@@ -23,19 +24,26 @@ fun FindItemsByLotIdsRequest.convert(): Result<FindItemsByLotIdsCommand, JsonErr
     return FindItemsByLotIdsCommand(
         cpid = cpid,
         ocid = ocid,
-        tender = this.tender.let { tender ->
-            FindItemsByLotIdsCommand.Tender(
-                lots = tender.lots.map { lot ->
-                    LotId.orNull(lot.id)
-                        ?: return failure(
-                            JsonErrors.DataFormatMismatch(
-                                path = "#/tender/lot",
-                                actualValue = lot.id,
-                                expectedFormat = LotId.pattern
-                            )
-                        )
-                }
-            )
-        }
+        tender = this.tender.convert("#tender")
+            .onFailure { return it }
     ).asSuccess()
+}
+
+fun FindItemsByLotIdsRequest.Tender.convert(path: String): Result<FindItemsByLotIdsCommand.Tender, JsonErrors> {
+
+    val lots = this.lots
+        .failureIfEmpty { return failure(JsonErrors.EmptyArray(path = "$path/lots")) }
+        .mapIndexed { idx, lot ->
+            LotId.orNull(lot.id)
+                ?: return failure(
+                    JsonErrors.DataFormatMismatch(
+                        path = "${path}/lots[${idx}]",
+                        actualValue = lot.id,
+                        expectedFormat = LotId.pattern
+                    )
+                )
+        }
+
+    return FindItemsByLotIdsCommand.Tender(lots = lots)
+        .asSuccess()
 }
