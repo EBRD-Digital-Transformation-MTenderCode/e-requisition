e45a70a1eea4837de3dbc45243beade5da1c7281
katherine <kate.shaidurova@gmail.com>
2020-10-30 13:25:51 +0200

    ES-4569 Added converters

---
+++ src/main/kotlin/com/procurement/requisition/infrastructure/handler/v1/get/award/ToGetAwardCriteriaAndConversionsResponseConverter.kt
@@ -0,0 +1,29 @@
+package com.procurement.requisition.infrastructure.handler.v1.get.award
+
+import com.procurement.requisition.application.service.get.award.model.GetAwardCriteriaAndConversionsResult
+import com.procurement.requisition.infrastructure.handler.v1.get.award.model.GetAwardCriteriaAndConversionsResponse
+
+fun GetAwardCriteriaAndConversionsResult.convert() = GetAwardCriteriaAndConversionsResponse(
+
+    awardCriteria = this.awardCriteria,
+    awardCriteriaDetails = this.awardCriteriaDetails,
+    conversions = conversions
+        ?.map { conversion ->
+            GetAwardCriteriaAndConversionsResponse.Conversion(
+                id = conversion.id,
+                relatesTo = conversion.relatesTo,
+                relatedItem = conversion.relatedItem,
+                description = conversion.description,
+                rationale = conversion.rationale,
+                coefficients = conversion.coefficients
+                    .map { coefficient ->
+                        GetAwardCriteriaAndConversionsResponse.Conversion.Coefficient(
+                            id = coefficient.id,
+                            value = coefficient.value,
+                            coefficient = coefficient.coefficient
+                        )
+                    }
+            )
+        }
+
+)
