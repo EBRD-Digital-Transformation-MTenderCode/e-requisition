2a3ce3f4e773d3d096671ce7089b5fef3bfec6a1
katherine <kate.shaidurova@gmail.com>
2020-10-30 16:16:49 +0200

    ES-4585 Added Value

--- src/main/kotlin/com/procurement/requisition/domain/model/tender/lot/Lot.kt
+++ src/main/kotlin/com/procurement/requisition/domain/model/tender/lot/Lot.kt
@@ -2,6 +2,7 @@ package com.procurement.requisition.domain.model.tender.lot
 
 import com.procurement.requisition.domain.model.EntityBase
 import com.procurement.requisition.domain.model.tender.Classification
+import com.procurement.requisition.domain.model.tender.Value
 
 data class Lot(
     override val id: LotId,
@@ -11,5 +12,6 @@ data class Lot(
     val status: LotStatus,
     val statusDetails: LotStatusDetails,
     val classification: Classification,
-    val variants: Variants
+    val variants: Variants,
+    val value: Value?
 ) : EntityBase<LotId>()
