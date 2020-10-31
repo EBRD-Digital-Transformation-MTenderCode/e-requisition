97f0962450aa9d1a1337914d694b2cd1e4a44138
Maxim Sambulat <maxim.sambulat@gmail.com>
2020-10-31 10:50:24 +0200

    ES-4472 Added test for 'GetAwardCriteriaAndConversionsResponse'.

---
+++ src/test/kotlin/com/procurement/requisition/infrastructure/handler/v1/get/GetAwardCriteriaAndConversionsResponseTest.kt
@@ -0,0 +1,13 @@
+package com.procurement.requisition.infrastructure.handler.v1.get
+
+import com.procurement.requisition.infrastructure.handler.v1.get.award.model.GetAwardCriteriaAndConversionsResponse
+import com.procurement.requisition.json.testingBindingAndMapping
+import org.junit.jupiter.api.Test
+
+class GetAwardCriteriaAndConversionsResponseTest {
+
+    @Test
+    fun fully() {
+        testingBindingAndMapping<GetAwardCriteriaAndConversionsResponse>("json/infrastructure/handler/v1/get/response_get_award_criteria_and_conversions.json")
+    }
+}
