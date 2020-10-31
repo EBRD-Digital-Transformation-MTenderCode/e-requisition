4d062ec3ad0bbd308d3387b048e5ae2a6b17d5f7
Maxim Sambulat <maxim.sambulat@gmail.com>
2020-10-31 10:49:56 +0200

    ES-4472 Removed annotation of Deserialize/Serialize 'CoefficientRate' type.

--- src/main/kotlin/com/procurement/requisition/infrastructure/handler/v1/get/award/model/GetAwardCriteriaAndConversionsResponse.kt
+++ src/main/kotlin/com/procurement/requisition/infrastructure/handler/v1/get/award/model/GetAwardCriteriaAndConversionsResponse.kt
@@ -2,17 +2,8 @@ package com.procurement.requisition.infrastructure.handler.v1.get.award.model
 
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonProperty
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize
-import com.fasterxml.jackson.databind.annotation.JsonSerialize
-import com.procurement.requisition.domain.model.award.AwardCriteria
-import com.procurement.requisition.domain.model.award.AwardCriteriaDetails
-import com.procurement.requisition.domain.model.tender.conversion.ConversionId
-import com.procurement.requisition.domain.model.tender.conversion.ConversionRelatesTo
-import com.procurement.requisition.domain.model.tender.conversion.coefficient.CoefficientId
 import com.procurement.requisition.domain.model.tender.conversion.coefficient.CoefficientRate
 import com.procurement.requisition.domain.model.tender.conversion.coefficient.CoefficientValue
-import com.procurement.requisition.infrastructure.bind.coefficient.CoefficientRateDeserializer
-import com.procurement.requisition.infrastructure.bind.coefficient.CoefficientRateSerializer
 
 data class GetAwardCriteriaAndConversionsResponse(
     @field:JsonProperty("awardCriteria") @param:JsonProperty("awardCriteria") val awardCriteria: String,
@@ -37,9 +28,6 @@ data class GetAwardCriteriaAndConversionsResponse(
         data class Coefficient(
             @field:JsonProperty("id") @param:JsonProperty("id") val id: String,
             @field:JsonProperty("value") @param:JsonProperty("value") val value: CoefficientValue,
-
-            @JsonDeserialize(using = CoefficientRateDeserializer::class)
-            @JsonSerialize(using = CoefficientRateSerializer::class)
             @field:JsonProperty("coefficient") @param:JsonProperty("coefficient") val coefficient: CoefficientRate
         )
     }
