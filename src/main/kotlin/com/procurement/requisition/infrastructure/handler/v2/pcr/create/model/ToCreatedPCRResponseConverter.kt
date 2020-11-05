8e0b74340dff259c4e16a047cc17b7501c6de123
Evgen Kopach <zkopach@gmail.com>
2020-11-05 13:26:52 +0200

    ES-4779 Replaced .toStringOrNull with .toString method.

--- src/main/kotlin/com/procurement/requisition/infrastructure/handler/v2/pcr/create/model/ToCreatedPCRResponseConverter.kt
+++ src/main/kotlin/com/procurement/requisition/infrastructure/handler/v2/pcr/create/model/ToCreatedPCRResponseConverter.kt
@@ -2,7 +2,6 @@ package com.procurement.requisition.infrastructure.handler.v2.pcr.create.model
 
 import com.procurement.requisition.application.service.create.pcr.model.CreatePCRResult
 import com.procurement.requisition.infrastructure.handler.converter.asString
-import com.procurement.requisition.infrastructure.handler.converter.asStringOrNull
 
 fun CreatePCRResult.convert() = CreatedPCRResponse(
     ocid = ocid.underlying,
@@ -57,7 +56,7 @@ fun CreatePCRResult.Tender.Lot.convert() = CreatedPCRResponse.Tender.Lot(
     title = title,
     description = description,
     status = status.asString(),
-    statusDetails = statusDetails?.asStringOrNull(),
+    statusDetails = statusDetails?.asString(),
     classification = classification.convert(),
     variants = variants.map { it.convert() },
 )
