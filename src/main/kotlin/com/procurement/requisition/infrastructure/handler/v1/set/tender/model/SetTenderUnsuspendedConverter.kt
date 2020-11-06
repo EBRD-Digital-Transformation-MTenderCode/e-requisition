c610367d6f1efd8c6f05906e5323f55db94c4b00
katherine <kate.shaidurova@gmail.com>
2020-11-06 15:42:08 +0200

    ES-4751 Replaced ProcurementMethodModality in response with String

--- src/main/kotlin/com/procurement/requisition/infrastructure/handler/v1/set/tender/model/SetTenderUnsuspendedConverter.kt
+++ src/main/kotlin/com/procurement/requisition/infrastructure/handler/v1/set/tender/model/SetTenderUnsuspendedConverter.kt
@@ -1,11 +1,12 @@
 package com.procurement.requisition.infrastructure.handler.v1.set.tender.model
 
 import com.procurement.requisition.application.service.set.tender.model.SetTenderUnsuspendedResult
+import com.procurement.requisition.infrastructure.handler.converter.asString
 
 fun SetTenderUnsuspendedResult.convert() = SetTenderUnsuspendedResponse(
     tender = SetTenderUnsuspendedResponse.Tender(
         status = tender.status,
         statusDetails = tender.statusDetails,
-        procurementMethodModalities = tender.procurementMethodModalities
+        procurementMethodModalities = tender.procurementMethodModalities.map { it.asString() }
     )
 )
--- src/main/kotlin/com/procurement/requisition/infrastructure/handler/v1/set/tender/model/SetTenderUnsuspendedResponse.kt
+++ src/main/kotlin/com/procurement/requisition/infrastructure/handler/v1/set/tender/model/SetTenderUnsuspendedResponse.kt
@@ -1,7 +1,6 @@
 package com.procurement.requisition.infrastructure.handler.v1.set.tender.model
 
 import com.fasterxml.jackson.annotation.JsonProperty
-import com.procurement.requisition.domain.model.tender.ProcurementMethodModality
 
 data class SetTenderUnsuspendedResponse(
     @field:JsonProperty("tender") @param:JsonProperty("tender") val tender: Tender
@@ -9,6 +8,6 @@ data class SetTenderUnsuspendedResponse(
     data class Tender(
         @field:JsonProperty("status") @param:JsonProperty("status") val status: String,
         @field:JsonProperty("statusDetails") @param:JsonProperty("statusDetails") val statusDetails: String,
-        @field:JsonProperty("procurementMethodModalities") @param:JsonProperty("procurementMethodModalities") val procurementMethodModalities: List<ProcurementMethodModality>
+        @field:JsonProperty("procurementMethodModalities") @param:JsonProperty("procurementMethodModalities") val procurementMethodModalities: List<String>
     )
 }
