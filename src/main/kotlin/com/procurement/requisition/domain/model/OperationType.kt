9aca8990b4defca756c2d969943e091fe6c23e24
katherine <kate.shaidurova@gmail.com>
2021-01-26 18:52:30 +0200

    ES-5406 Added operationTypes withdrawPcrProtocol and completeSourcing

--- src/main/kotlin/com/procurement/requisition/domain/model/OperationType.kt
+++ src/main/kotlin/com/procurement/requisition/domain/model/OperationType.kt
@@ -4,11 +4,13 @@ import com.procurement.requisition.lib.enumerator.EnumElementProvider
 
 enum class OperationType(override val key: String) : EnumElementProvider.Element {
 
+    COMPLETE_SOURCING("completeSourcing"),
     CREATE_PCR("createPcr"),
     PCR_PROTOCOL("pcrProtocol"),
     SUBMIT_BID_IN_PCR("submitBidInPcr"),
     TENDER_PERIOD_END_AUCTION_IN_PCR("tenderPeriodEndAuctionInPcr"),
-    TENDER_PERIOD_END_IN_PCR("tenderPeriodEndInPcr");
+    TENDER_PERIOD_END_IN_PCR("tenderPeriodEndInPcr"),
+    WITHDRAW_PCR_PROTOCOL("withdrawPcrProtocol");
 
     override fun toString(): String = key
 
