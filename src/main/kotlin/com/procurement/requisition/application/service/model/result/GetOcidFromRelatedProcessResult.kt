94c61c9f0f8ca3292aca337494425372ff3c7c59
katherine <kate.shaidurova@gmail.com>
2021-01-29 11:24:27 +0200

    ES-5418 Added DTOs

---
+++ src/main/kotlin/com/procurement/requisition/application/service/model/result/GetOcidFromRelatedProcessResult.kt
@@ -0,0 +1,5 @@
+package com.procurement.requisition.application.service.model.result
+
+import com.procurement.requisition.domain.model.Ocid
+
+data class GetOcidFromRelatedProcessResult(val ocid: Ocid)
