a73b2e4155ebbbcec09bd6cd126823ddaf0ae2b1
katherine <kate.shaidurova@gmail.com>
2021-01-29 18:19:05 +0200

    ES-5418 Added service

---
+++ src/main/kotlin/com/procurement/requisition/application/service/GetOcidFromRelatedProcessService.kt
@@ -0,0 +1,45 @@
+package com.procurement.requisition.application.service
+
+import com.procurement.requisition.application.service.error.GetOcidFromRelatedProcessErrors
+import com.procurement.requisition.application.service.model.OperationTypeGetOcidFromRelatedProcess
+import com.procurement.requisition.application.service.model.command.GetOcidFromRelatedProcessCommand
+import com.procurement.requisition.application.service.model.result.GetOcidFromRelatedProcessResult
+import com.procurement.requisition.domain.model.Ocid
+import com.procurement.requisition.domain.model.relatedprocesses.RelatedProcess
+import com.procurement.requisition.domain.model.relatedprocesses.Relationship
+import com.procurement.requisition.lib.fail.Failure
+import com.procurement.requisition.lib.functional.Result
+import com.procurement.requisition.lib.functional.asFailure
+import org.springframework.stereotype.Service
+
+@Service
+class GetOcidFromRelatedProcessService(
+    private val pcrManagement: PCRManagementService,
+) {
+
+    fun get(command: GetOcidFromRelatedProcessCommand): Result<GetOcidFromRelatedProcessResult, Failure> {
+        val pcr = pcrManagement.find(cpid = command.cpid, ocid = command.ocid)
+            .onFailure { return it }
+            ?: return GetOcidFromRelatedProcessErrors.PCRNotFound(cpid = command.cpid, ocid = command.ocid)
+                .asFailure()
+
+        val appropriateRelationship = getAppropriateRelationship(command.operationType)
+
+        val relatedProcess = pcr.relatedProcesses
+            .firstOrNull { relatedProcess -> containsRelationship(relatedProcess, appropriateRelationship) }
+            ?: return GetOcidFromRelatedProcessErrors.RelationshipNotFound(appropriateRelationship)
+                .asFailure()
+
+        val relatedProcessOcid = Ocid.SingleStage.tryCreateOrNull(relatedProcess.identifier)!!
+
+        return Result.success(GetOcidFromRelatedProcessResult(relatedProcessOcid))
+    }
+
+    private fun getAppropriateRelationship(operationType: OperationTypeGetOcidFromRelatedProcess): Relationship =
+        when (operationType) {
+            OperationTypeGetOcidFromRelatedProcess.COMPLETE_SOURCING -> Relationship.X_FRAMEWORK
+        }
+
+    private fun containsRelationship(relatedProcess: RelatedProcess, relationship: Relationship) =
+        relatedProcess.relationship.any { it == relationship }
+}
