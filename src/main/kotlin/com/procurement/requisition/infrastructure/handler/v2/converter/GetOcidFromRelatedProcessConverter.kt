e866c7e8957dc619e6b238f1edc5619809864e1f
katherine <kate.shaidurova@gmail.com>
2021-02-01 12:15:09 +0200

    ES-5418 Removed redundant allowedOperationTypes

--- src/main/kotlin/com/procurement/requisition/infrastructure/handler/v2/converter/GetOcidFromRelatedProcessConverter.kt
+++ src/main/kotlin/com/procurement/requisition/infrastructure/handler/v2/converter/GetOcidFromRelatedProcessConverter.kt
@@ -1,6 +1,5 @@
 package com.procurement.requisition.infrastructure.handler.v2.converter
 
-import com.procurement.requisition.application.service.model.OperationType
 import com.procurement.requisition.application.service.model.OperationTypeGetOcidFromRelatedProcess
 import com.procurement.requisition.application.service.model.command.GetOcidFromRelatedProcessCommand
 import com.procurement.requisition.domain.failure.error.JsonErrors
@@ -11,21 +10,6 @@ import com.procurement.requisition.infrastructure.handler.converter.asSingleStag
 import com.procurement.requisition.infrastructure.handler.v2.model.request.GetOcidFromRelatedProcessRequest
 import com.procurement.requisition.lib.functional.Result
 import com.procurement.requisition.lib.functional.asSuccess
-
-private val allowedOperationTypes = OperationType.allowedElements
-    .asSequence()
-    .filter {
-        when (it) {
-            OperationType.COMPLETE_SOURCING -> true
-            OperationType.CREATE_PCR,
-            OperationType.PCR_PROTOCOL,
-            OperationType.SUBMIT_BID_IN_PCR,
-            OperationType.TENDER_PERIOD_END_AUCTION_IN_PCR,
-            OperationType.TENDER_PERIOD_END_IN_PCR,
-            OperationType.WITHDRAW_PCR_PROTOCOL -> false
-        }
-    }
-    .toSet()
 
 fun GetOcidFromRelatedProcessRequest.convert(): Result<GetOcidFromRelatedProcessCommand, JsonErrors> {
     val cpid = cpid.asCpid().onFailure { return it.repath(path = "/cpid") }
