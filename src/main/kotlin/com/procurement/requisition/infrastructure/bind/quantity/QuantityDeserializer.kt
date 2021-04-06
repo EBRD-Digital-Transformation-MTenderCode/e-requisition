ac6b1ae69ae8849b5c182037cf59c64efc50d16f
Evgen Kopach <zkopach@gmail.com>
2021-04-06 14:53:00 +0300

    ES-5921 Fixed Quantity deserializer. Throw exception when scale greater than allowed

--- src/main/kotlin/com/procurement/requisition/infrastructure/bind/quantity/QuantityDeserializer.kt
+++ src/main/kotlin/com/procurement/requisition/infrastructure/bind/quantity/QuantityDeserializer.kt
@@ -11,8 +11,13 @@ import java.math.BigDecimal
 
 class QuantityDeserializer : JsonDeserializer<BigDecimal>() {
     companion object {
+        private val MAX_ALLOWED_SCALE = 3
         fun deserialize(text: String): BigDecimal = try {
-            BigDecimal(text).setScale(3)
+            val scale = BigDecimal(text).scale()
+            if (scale > MAX_ALLOWED_SCALE)
+                throw IllegalArgumentException("Attribute 'quantity' is an invalid scale '$scale', the maximum scale: '$MAX_ALLOWED_SCALE'.")
+
+            BigDecimal(text).setScale(MAX_ALLOWED_SCALE)
         } catch (exception: Exception) {
             throw QuantityValueException(quantity = text, description = exception.message ?: "")
         }
